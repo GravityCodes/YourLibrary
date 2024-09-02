f8c877aad2eec4394a86776e6b1274a38eb64664
Johan Mesa <Johanotr@gmail.com>
2024-09-01 22:11:40 -0400

    Init Project

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+// Book  function constructor
+function Book(name, author, pages, read){
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+
+function addBookToLibrary() {
+    
+}
